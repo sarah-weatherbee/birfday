d8b8d0de3c24279ef04fafd34608faadfba4d80c
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-05-20 21:27:10 -0500

    authentication working

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,10 +4,13 @@ import 'bootstrap';
 import Auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
 import apiKeys from './helpers/apiKeys.json';
-
+import authData from './helpers/data/authData';
+import MyNavbar from './components/MyNavbar/MyNavbar';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  MyNavbar.navbarEvents();
+  authData.checkLoginStatus();
   Auth.authStringBuilder();
   birfday.birfdayStringBuilder();
 };
