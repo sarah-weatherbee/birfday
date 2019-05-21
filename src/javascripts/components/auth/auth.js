197ad8586b9704663c82f96a7c1b3f90ef910871
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-05-20 19:55:44 -0500

    initialized firebase

---
+++ src/javascripts/components/auth/auth.js
@@ -0,0 +1,8 @@
+import util from '../../helpers/util';
+
+const authStringBuilder = () => {
+  const domString = 'Auth';
+  util.printToDom('auth', domString);
+};
+
+export default { authStringBuilder };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,15 @@
+import firebase from 'firebase/app';
 import '../styles/main.scss';
 import 'bootstrap';
+import Auth from './components/auth/auth';
+import birfday from './components/birfday/birfday';
+import apiKeys from './helpers/apiKeys.json';
 
-console.error('hi');
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  Auth.authStringBuilder();
+  birfday.birfdayStringBuilder();
+};
+
+init();
