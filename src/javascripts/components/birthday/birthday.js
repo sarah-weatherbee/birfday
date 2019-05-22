329efeca1e9b1c792616c2912c1b013394bab4d3
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-05-22 14:07:57 -0500

    corrected spelling of birfday to birthday so it's clear I can spell

---
+++ src/javascripts/components/birthday/birthday.js
@@ -0,0 +1,8 @@
+import util from '../../helpers/util';
+
+const birthdayStringBuilder = () => {
+  const domString = 'Birthday';
+  util.printToDom('birthday', domString);
+};
+
+export default { birthdayStringBuilder };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import '../styles/main.scss';
 import 'bootstrap';
 import Auth from './components/auth/auth';
-import birfday from './components/birfday/birfday';
+import birthday from './components/birthday/birthday';
 import apiKeys from './helpers/apiKeys.json';
 import authData from './helpers/data/authData';
 import MyNavbar from './components/MyNavbar/MyNavbar';
@@ -12,7 +12,7 @@ const init = () => {
   MyNavbar.navbarEvents();
   authData.checkLoginStatus();
   Auth.authStringBuilder();
-  birfday.birfdayStringBuilder();
+  birthday.birthdayStringBuilder();
 };
 
 init();
