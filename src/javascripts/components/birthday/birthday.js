488a9c14fca246f7edd44886b6ff1bc49a6e129a
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-05-25 11:31:24 -0500

    birthday 1 prints to dom

--- src/javascripts/components/birthday/birthday.js
+++ src/javascripts/components/birthday/birthday.js
@@ -1,8 +1,15 @@
+import birthdayData from '../../helpers/data/birthdayData';
 import util from '../../helpers/util';
 
-const birthdayStringBuilder = () => {
-  const domString = 'Birthday';
-  util.printToDom('birthday', domString);
+const birthdayStringBuilder = (uid) => {
+  birthdayData.getBirthdayByUid(uid)
+    .then((birthday) => {
+      let domString = `<h1>${birthday.date}</h1>`;
+      domString += `<img src=${birthday.imageUrl} alt="birthday location">`;
+      domString += `<h2>${birthday.location} @ ${birthday.time}</h2>`;
+      util.printToDom('event', domString);
+    })
+    .catch(err => console.error('could not get birthday', err));
 };
 
 export default { birthdayStringBuilder };
